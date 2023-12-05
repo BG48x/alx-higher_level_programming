ef4f472bba489198d436ba598a39dc570ba4200f
BG48x <binu86226@gmail.com>
2023-12-05 05:48:53 -0800

    5-no_c.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def primt_matric_Integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col). end=" " if col != row[-1] else "")
-        print()
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+        for i in range(len(submatrix)):
+            print("{:d}".format(submatrix[i]),
+                    end="\n" if i is len(submatrix) - 1 else " ")
+
